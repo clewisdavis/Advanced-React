9766fc71bcc8fbca1b98f03a1a9bc501d0d01959
Chris Davis <chrislewis.davis@gmail.com>
2023-02-15 08:37:47 -0500

    order page

---
+++ sick-fits/frontend/pages/order/[id].js
@@ -0,0 +1,42 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import ErrorMessage from '../../components/ErrorMessage';
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order: Order(where: { id: $id }) {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        Id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleOrderPage({ query }) {
+  const { data, error, loading } = useQuery(SINGLE_ORDER_QUERY, {
+    variables: { id: query.id },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+  const { order } = data;
+  return (
+    <div>
+      Hey {order.total} {order.charge}
+    </div>
+  );
+}
